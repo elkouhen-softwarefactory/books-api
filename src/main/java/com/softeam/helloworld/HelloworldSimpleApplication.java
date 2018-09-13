16c9541ada959aa52303f5e4af5188280e2df281
EL KOUHEN Mehdi <mehdi.elkouhen@gmail.com>
2018-09-13 15:35:33 +0200

    wip

--- src/main/java/com/softeam/helloworld/HelloworldSimpleApplication.java
+++ src/main/java/com/softeam/helloworld/HelloworldSimpleApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloworldApplication {
+public class HelloworldSimpleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloworldApplication.class, args);
+        SpringApplication.run(HelloworldSimpleApplication.class, args);
     }
 }
