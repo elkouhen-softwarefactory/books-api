3927173037be3dd1b392e738365011def6725fe3
EL KOUHEN Mehdi <mehdi.elkouhen@gmail.com>
2019-01-08 22:34:16 +0100

    wip

--- src/main/resources/db/migration/V1_0__init.sql
+++ src/main/resources/db/migration/V1_0__init.sql
@@ -10,8 +10,8 @@ INSERT INTO T_BOOK (ID, TITLE, AUTHORS) VALUES (1, 'Design Pattern', 'Erich Gamm
 
 INSERT INTO T_BOOK (ID, TITLE, AUTHORS) VALUES (2, 'Effective Java', 'Joshua Bloch');
 
-INSERT INTO T_BOOK (ID, TITLE, AUTHORS) VALUES (3, 'The C++ Programming Language', 'Bjarne Stroustrup');
+INSERT INTO T_BOOK (ID, TITLE, AUTHORS) VALUES (3, 'Specification by Example', 'Gojko Adzic');
 
 INSERT INTO T_BOOK (ID, TITLE, AUTHORS) VALUES (4, 'The Clean Coder: A Code of Conduct for Professional Programmers', 'Robert C. Martin');
 
-INSERT INTO T_BOOK (ID, TITLE, AUTHORS) VALUES (5, 'The Clean Coder: A Code of Conduct for Professional Programmers', 'Robert C. Martin');
+-- INSERT INTO T_BOOK (ID, TITLE, AUTHORS) VALUES (5, 'Refactoring', 'Martin Fowler');
