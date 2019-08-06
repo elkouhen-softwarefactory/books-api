a098995a847e493f6f48dcb974fa5c17e64dca5f
Mehdi EL KOUHEN <melkouhen@ippon.fr>
2019-08-06 13:58:54 +0200

    wip

---
+++ resources/db/migration/V1_0__init.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS T_BOOK;
+
+CREATE TABLE T_BOOK (
+  ID       INTEGER      NOT NULL PRIMARY KEY,
+  TITLE    VARCHAR(128) NOT NULL,
+  AUTHORS   VARCHAR(128)
+);
+
+INSERT INTO T_BOOK (ID, TITLE, AUTHORS) VALUES (1, 'Design Pattern', 'Erich Gamma, John Vlissides, Ralph E.. Johnson et Richard Helm');
+
+INSERT INTO T_BOOK (ID, TITLE, AUTHORS) VALUES (2, 'Effective Java', 'Joshua Bloch');
+
+INSERT INTO T_BOOK (ID, TITLE, AUTHORS) VALUES (3, 'Specification by Example', 'Gojko Adzic');
+
+INSERT INTO T_BOOK (ID, TITLE, AUTHORS) VALUES (4, 'The Clean Coder: A Code of Conduct for Professional Programmers', 'Robert C. Martin');
+
+INSERT INTO T_BOOK (ID, TITLE, AUTHORS) VALUES (5, 'Refactoring', 'Martin Fowler');
